bcc5c34a754574b5892fb202ac90def34ddf7f3a
Samuel Gratzl <sam@sgratzl.com>
2020-06-04 16:55:27 +0200

    simplify

--- src/simplifiers/ShapeSimplifier.ts
+++ src/simplifiers/ShapeSimplifier.ts
@@ -1,48 +1,23 @@
 import { PointPath } from '../PointPath';
 import { linePtSegDistSq } from '../utils';
 
-class State {
-  end: number;
-  constructor(private readonly path: PointPath, private readonly start: number) {
-    this.end = start + 1;
+function canTakeNext(path: PointPath, start: number, end: number, toleranceSquared: number) {
+  const validEnd = path.closed ? end < path.length : end < path.length - 1;
+  if (!validEnd) {
+    return false;
   }
 
-  advanceEnd() {
-    this.end += 1;
-  }
-  decreaseEnd() {
-    this.end -= 1;
-  }
-  validEnd() {
-    return this.path.closed ? this.end < this.path.length : this.end < this.path.length - 1;
-  }
-  endPoint() {
-    return this.path.get(this.end);
-  }
-  startPoint() {
-    return this.path.get(this.start);
-  }
-  lineDstSqr(ix: number) {
-    const p = this.path.get(ix);
-    const s = this.startPoint();
-    const e = this.endPoint();
-    return linePtSegDistSq(s[0], s[1], e[0], e[1], p[0], p[1]);
-  }
-  canTakeNext(toleranceSquared: number) {
-    if (!this.validEnd()) {
+  const s = path.get(start);
+  const e = path.get(end + 1);
+
+  for (let index = start + 1; index <= end; index++) {
+    const p = path.get(index);
+    const len = linePtSegDistSq(s[0], s[1], e[0], e[1], p[0], p[1]);
+    if (len > toleranceSquared) {
       return false;
     }
-    let ok = true;
-    this.advanceEnd();
-    for (let ix = this.start + 1; ix < this.end; ix += 1) {
-      if (this.lineDstSqr(ix) > toleranceSquared) {
-        ok = false;
-        break;
-      }
-    }
-    this.decreaseEnd();
-    return ok;
   }
+  return true;
 }
 
 export function shapeSimplifier(tolerance = 0.0) {
@@ -54,12 +29,12 @@ export function shapeSimplifier(tolerance = 0.0) {
     let start = 0;
     const toleranceSquared = tolerance * tolerance;
     while (start < path.length) {
-      var s = new State(path, start);
-      while (s.canTakeNext(toleranceSquared)) {
-        s.advanceEnd();
+      let end = start + 1;
+      while (canTakeNext(path, start, end, toleranceSquared)) {
+        end++;
       }
-      start = s.end;
-      points.push(s.startPoint());
+      points.push(path.get(start));
+      start = end;
     }
     return new PointPath(points);
   };
